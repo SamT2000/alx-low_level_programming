083bb3b431d80853b7bbe1d7938fc7284c8d681c
SamT2000 <SamT2000@gmail.com>
2022-12-13 11:28:23 -0800

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value
+ *
+ * @n: takes integer type input
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
