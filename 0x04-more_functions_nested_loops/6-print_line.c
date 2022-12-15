cd06a5040702390040363d828c39a74ed6c58ae9
SamT2000 <SamT2000@gmail.com>
2022-12-15 12:05:22 -0800

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints a line on the terminal
+ *
+ * @n: takes interger type input
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
