960275c8c2923bf5f057a280c1e44bf60a250ef9
SamT2000 <SamT2000@gmail.com>
2023-01-05 11:49:29 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: takes string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i -= 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
